b6dc64d87fcbea0ad4073fe56bf86bd1ca768977
anoopvt <armia247>
2023-06-22 14:46:44 +0530

    edited
    Signed-off-by: anoopvt <armia247>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Header from "./component/Layout/Header";
 import { Fragment } from "react";
-
+import Meals from "./component/Meals/Meals";
 function App() {
   return (
     <Fragment>
       <Header></Header>
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
