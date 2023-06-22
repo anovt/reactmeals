db35a9a465a6a630d51f0d8046b8f0424164482b
anoopvt <armia247>
2023-06-22 21:31:24 +0530

    edited
    Signed-off-by: anoopvt <armia247>

--- src/component/Meals/AvailableMeals.js
+++ src/component/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -29,14 +30,20 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const MealList = DUMMY_MEALS.map((meal) => {
-    return <li>{meal.name}</li>;
+    return (
+      <MealItem
+        key={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    );
   });
   return (
     <section className={classes.meals}>
       <Card>
-      <ul>{MealList}</ul>
+        <ul>{MealList}</ul>
       </Card>
-      
     </section>
   );
 };
