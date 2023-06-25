100e7361808bfc7731b01700b7d86ab5b6dce0b7
anoopvt <armia247>
2023-06-25 23:20:28 +0530

    edited
    Signed-off-by: anoopvt <armia247>

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -4,11 +4,14 @@ import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
-
   const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", price: 12.99, amount: 2 }].map((item) => {
+      {cartCtx.items.map((item) => {
         return <li key={item.id}>{item.name}</li>;
       })}
     </ul>
@@ -19,13 +22,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button> }
       </div>
     </Modal>
   );
